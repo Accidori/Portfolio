80e0066e3542736270e95f4c8e407c96199c2ae5
Funkyskep <phyrowolf24@gmail.com>
2024-11-24 14:36:27 -0500

    testing

--- my-portfolio/src/components/Resume.tsx
+++ my-portfolio/src/components/Resume.tsx
@@ -1,40 +1,28 @@
 import React from 'react';
 
 
+
+//i dont really have much to put here, havent worked a job yet :/
 const Resume: React.FC = () => {
     return (
         <section className="resume">
             <h1>Resume</h1>
-                <h2>Skills</h2>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Node.js</li>
-                        <li>Express.js</li>
-                        <li>SQL</li>
-                    </ul>
-                <h2>Experience</h2>
-                    <h3>Full Stack Developer</h3>
-                    <p>Developed full stack applications using React, Node.js, and SQL.</p>
-
-
-                <h2>Education</h2>
-                    <h3>Full Stack Developer</h3>
-                    <p>University of Arizona</p>
-                    <h3>Front End Developer</h3>
-                    <p>University of Arizona</p>
-                    <h3>Back End Developer</h3>
-                    <p>University of Arizona</p>
-            </div>
+            <h2>Skills</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+            </ul>
+            <h2>Education</h2>
+            <h3>Standard Education</h3>
+            <p>Rahway High</p>
         </section>
-
-
-
-
     );
-}
+};
 
 
-// export default Resume;
+export default Resume;
